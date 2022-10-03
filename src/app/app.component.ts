8da93596c3e45f895cd538485c892d149bddd097
mtorres91 <m.a.t.a.9125_@hotmail.com>
2022-10-03 16:31:11 -0500

    nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import Validation from './utils/validation';
+
 
 
 @Component({
@@ -8,9 +17,141 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formof';
+
+  
+options: Array<any> = [
+    { name: 'Si', value: 'Si' },
+    { name: 'No', value: 'No' }
+];
+
+optionslive: Array<any> = [
+  { name: 'Área metropolitana de la Ciudad de México', value: 'CDMX' },
+  { name: 'Área metropolitana de Guadalajara', value: 'GDL' },
+  { name: 'Otro', value: 'OTRO' }
+];
+
+  formof: FormGroup = new FormGroup({
+    prestamo: new FormControl(''),
+    ingreso: new FormControl(''),
+    gastos: new FormControl(''),
+    mayor: new FormControl(''),
+    propietario: new FormControl(''),
+    ubicacion: new FormControl(''),
+    atrasos: new FormControl('')
+  });
+
+
+  form: FormGroup = new FormGroup({
+    fullname: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    acceptTerms: new FormControl(false),
+  });
+
+  
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+
+    //formof//
+    this.formof = this.formBuilder.group(
+      {
+        prestamo: ['', Validators.required],
+        ingreso: ['', Validators.required],
+        gastos: ['', Validators.required],
+        mayor: [null, [Validators.required]],
+        propietario: [null, [Validators.required]],
+        ubicacion: [null, [Validators.required]],
+        atrasos: [null, [Validators.required]]
+      }
+    );
+
+
+    ///ejemplo//
+    this.form = this.formBuilder.group(
+      {
+        fullname: ['', Validators.required],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')],
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  get fom(): { [key: string]: AbstractControl } {
+    return this.formof.controls;
+  }
+
+  changemayor(event: any) {
+    console.log(event.value);
+  }
+
+  changepropietario(event: any) {
+    console.log(event.value);
+  }
+
+  changeubicacion(event: any) {
+    console.log(event.value);
+  }
+  changeatrasos(event: any) {
+    console.log(event.value);
+  }
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onSubmit2(): void {
+    this.submitted = true;
+
+    if (this.formof.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.formof.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+  
 }
 export class CardSubtitleExample {
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule}from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 
 
 import {MatIcon, MatIconModule}from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 
 @NgModule({
@@ -18,16 +23,22 @@ import {MatInputModule} from '@angular/material/input';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatRadioModule
     
     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
